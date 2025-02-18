1a1648ca6379c8e0d3b76d23ea84766eb2a071f3
vJhonatan <jhonatan99653@gmail.com>
2025-02-18 19:29:59 -0300

    initial commit

---
+++ src/main/java/com/dev/NailFlow/NailFlowApplication.java
@@ -0,0 +1,13 @@
+package com.dev.NailFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NailFlowApplication.class, args);
+	}
+
+}
